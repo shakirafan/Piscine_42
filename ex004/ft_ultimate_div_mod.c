b68af171dbfbb451214e36d185797c58730fa29e
shakirafan <alexandrealbeza@gmail.com>
2017-01-18 13:20:47 +0100

    rm

--- ex004/ft_ultimate_div_mod.c
+++
@@ -1,8 +0,0 @@
-void	ft_ultimate_div_mod(int *a, int *b)
-{
-	int stock;
-
-	stock = *a;
-	*a = *a / *b;
-	*b = stock % *b;
-}
--- ex005/ft_putstr.c
+++
@@ -1,8 +0,0 @@
-void	ft_putstr(char *str)
-{
-	while (str[0] != '\0')
-	{
-		ft_putchar(str[0]);
-		str++;
-	}
-}
